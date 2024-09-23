6fba2e0a5d6f9f85023e38928f93ba2185a38ea4
antoineuldry <antoine.uldry@heig-vd.ch>
2024-09-23 16:20:01 +0200

    Loop done

--- src/class/Circle.js
+++ src/class/Circle.js
@@ -1,39 +1,42 @@
-import { TAU } from '../utils/math.js';
+import { TAU } from "../utils/math.js";
 
 export default class Circle {
+	constructor({
+		x = 0,
+		y = 0,
+		radius = 100,
+		color = "tomato",
+		speed = 1000,
+		direction = TAU / 4,
+	} = {}) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = color;
+		this.speed = speed;
+		this.direction = direction;
+	}
 
-  constructor({
-    x = 0,
-    y = 0,
-    radius = 100,
-    color = 'tomato',
-    speed = 100,
-    direction = TAU / 4
-  } = {}) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
-    this.speed = speed;
-    this.direction = direction;
-  }
+	setDir(angle) {
+		this.direction = angle;
+	}
 
-  setDir(angle) {
-    this.direction = angle;
-  }
+	move(dt, width, height) {
+		const dx = (this.speed * Math.cos(this.direction) * dt) / 1000;
+		const dy = (this.speed * Math.sin(this.direction) * dt) / 1000;
+		this.x += dx;
+		this.y += dy;
 
-  move(dt) {
-    const dx = this.speed * Math.cos(this.direction) * dt / 1000;
-    const dy = this.speed * Math.sin(this.direction) * dt / 1000;
-    this.x += dx;
-    this.y += dy;
-  }
+		if (this.x + this.radius < 0) this.x = width + this.radius;
+		if (this.x - this.radius > width) this.x = -this.radius;
+		if (this.y + this.radius < 0) this.y = height + this.radius;
+		if (this.y - this.radius > height) this.y = -this.radius;
+	}
 
-  draw(ctx) {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, TAU);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
-
+	draw(ctx) {
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, TAU);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+	}
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Circle from "./class/Circle";
 import Keyboard from "./class/Keyboard";
 import { TAU } from "./utils/math";
 
-const ctx = document.querySelector('canvas').getContext('2d');
+const ctx = document.querySelector("canvas").getContext("2d");
 
 ctx.canvas.width = ctx.canvas.clientWidth;
 ctx.canvas.height = ctx.canvas.clientHeight;
@@ -10,45 +10,50 @@ ctx.canvas.height = ctx.canvas.clientHeight;
 const keyboard = new Keyboard();
 const circles = [];
 for (let i = 0; i < 100; i++) {
-  circles.push(new Circle({
-    x: Math.random() * ctx.canvas.width,
-    y: Math.random() * ctx.canvas.height,
-    radius: Math.random() * 80,
-    color: `hsl(${Math.random() * 360}, 100%, 50%)`,
-    speed: Math.random() * 100,
-    direction: 0,
-  }));
+	const radius = Math.random() * 80;
+	circles.push(
+		new Circle({
+			x: Math.random() * ctx.canvas.width,
+			y: Math.random() * ctx.canvas.height,
+			radius,
+			color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+			speed: radius * 7,
+			direction: 0,
+		})
+	);
 }
 
+circles.sort((a, b) => a.radius - b.radius);
+
 function tick(timestamp) {
-  const dt = timestamp - lastTickTime;
-  lastTickTime = timestamp;
+	const dt = timestamp - lastTickTime;
+	lastTickTime = timestamp;
 
-  // User input management
-  let angle = false;
-  if(keyboard.isKeyDown('KeyW')) {
-    angle = 3/4 * TAU;
-  } else if(keyboard.isKeyDown('KeyS')) {
-    angle = 1/4 * TAU;
-  }
+	// User input management
+	let angle = false;
+	if (keyboard.isKeyDown("KeyW")) {
+		angle = (3 / 4) * TAU;
+	} else if (keyboard.isKeyDown("KeyS")) {
+		angle = (1 / 4) * TAU;
+	}
 
-  if (angle !== false) {
-    for (const c of circles) {
-      c.setDir(angle);
-      c.move(dt);
-    }
-  }
+	if (angle !== false) {
+		for (const c of circles) {
+			c.setDir(angle);
+			c.move(dt, ctx.canvas.width, ctx.canvas.height);
+		}
+	}
 
-  // Draw the world
-  ctx.canvas.width = ctx.canvas.clientWidth;
-  ctx.canvas.height = ctx.canvas.clientHeight;
-  for (const c of circles) {
-    c.draw(ctx);
-  }
+	// Draw the world
+	ctx.canvas.width = ctx.canvas.clientWidth;
+	ctx.canvas.height = ctx.canvas.clientHeight;
+	for (const c of circles) {
+		c.draw(ctx);
+	}
 
-  // Game loop
-  requestAnimationFrame(tick);
+	// Game loop
+	requestAnimationFrame(tick);
 }
 
 let lastTickTime = 0;
-requestAnimationFrame(tick)
+requestAnimationFrame(tick);
